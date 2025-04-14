60f7b357a52161f6ffd88801a939688ea473a2b0
kheng2004 <khangkhang0619@gmail.com>
2025-04-14 22:08:54 +0700

    comment

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Feature(models.Model):
+    # def __init__(self, id, name, details, is_true):
+    #     self.id = id
+    #     self.name = name
+    #     self.details = details
+    #     self.is_true = is_true
+    name = models.CharField(max_length=100)
+    details = models.CharField(max_length=500)
